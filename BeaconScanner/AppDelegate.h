0857343507d9c3799bff5203fc08f7b980d82d93
Johan Haneveld <johan@rhinofly.nl>
2014-02-25 17:47:58 +0100

    Initial commit

---
+++ BeaconScanner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeaconScanner
+//
+//  Created by Johan Haneveld on 2/25/14.
+//  Copyright (c) 2014 Idlework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
